1e9209f050aa1b863a32192bfc9fcabfcac0ef0f
Castellano-Dan <yaizacastellano@icloud.com>
2024-11-05 19:47:01 -0600

    Agregamos la clase inventario con todos sus atributos y métodos.

---
+++ GestioInventario/Inventario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioInventario
+{
+    public class Inventario
+    
+    {
+        private List<Producto> productos;
+
+        public Inventario()
+        {
+            productos = new List<Producto>();
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public List<Producto> FiltrarProductos(decimal precioMinimo)
+        {
+            return productos.Where(p => p.Precio > precioMinimo)
+                            .OrderBy(p => p.Precio)
+                            .ToList();
+        }
+
+        public void MostrarProductos()
+        {
+            foreach (var producto in productos)
+            {
+                producto.MostrarInformacion();
+            }
+        }
+    }
+}
+
